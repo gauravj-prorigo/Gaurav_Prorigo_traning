a32133a3a6dff2d7cbd5cadebbe7cc15a46fd4fa
Your Name <your@email.com>
2025-10-07 15:49:17 +0530

    Added Week 1 assignments

---
+++ week1/week1_monday.py
@@ -0,0 +1,4 @@
+name = input("Enter your name :")
+age = input("Enter your age :")
+Native = input("Enter your Nativeinde :")
+print("Hello", name + "!" , "your are", age,"Years old. && adress is",Native)
